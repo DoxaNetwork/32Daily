169452a8a1e1b79309cf64ae3c046b19e84be9d2
Travis Hairfield <travis.hairfield@gmail.com>
2017-11-01 17:15:43 -0700

    delete css we aren't using

--- src/App.js
+++ src/App.js
@@ -102,15 +102,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Toasty</a>
-        </nav>
-
-        
-
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
+        <main>
+          <div>
+            <div>
               {this.props.children} - {this.props.name}
               <p>User count: {this.state.userCount}</p> 
               <button onClick={this.getUsers}>update user count</button>
