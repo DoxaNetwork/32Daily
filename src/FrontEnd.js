a89a7c8fb378815bc4d00890b7fee18d5bc81a42
Travis Hairfield <travis.hairfield@gmail.com>
2018-04-16 12:45:39 -0700

    connect word posting to ethereum

--- src/FrontEnd.js
+++ src/FrontEnd.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { getAllLinks, postLink } from './DappFunctions'
 
 import './FrontEnd.css'
 
@@ -6,7 +7,24 @@ class FrontEnd extends Component {
 
 	constructor(props){
 		super(props);
+		this.state = {'submittedWords': []}
 	}
+
+	async componentWillMount() {
+        const posts = await getAllLinks();
+        const postObjs = posts.map(postObj => this.mapPost(postObj))
+        this.setState({'submittedWords': postObjs})
+    }
+
+    mapPost(post) {
+    	return {'word': post.link, 'width': post.backing.toNumber()}
+    }
+
+    async postLink(content) {
+        const result = await postLink(content);
+        const newPost = this.mapPost(result.logs[0].args);
+        this.setState({ submittedWords: [...this.state.submittedWords, newPost] })
+    }
 
 	render() {
 		const publishedWords = this.props.publishedWords.map(word => 
@@ -16,7 +34,7 @@ class FrontEnd extends Component {
 			</div>
 		);
 
-		const submittedWords = this.props.submittedWords.map(obj =>
+		const submittedWords = this.state.submittedWords.map(obj =>
 			<SubmittedWord word={obj.word} width={obj.width} />
 		);
 
@@ -26,7 +44,9 @@ class FrontEnd extends Component {
 					{publishedWords}
 				</div>
 				<div className="submittedWords">
+					<NextWord onSubmit={this.postLink.bind(this)}/>
 					{submittedWords}
+					
 				</div>
 
 			</div>
@@ -51,7 +71,7 @@ class SubmittedWord extends Component {
 			return;
 		}
 
-		let newWidth = this.state.width * 1.5;
+		let newWidth = this.state.width + 30;
 
 		if (newWidth > (300 - 127)) {
 			newWidth = 300 - 127;
@@ -95,12 +115,35 @@ class NextWord extends Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {content: ''}
 	}
+
+	handleContentChange(event) {
+        this.setState({content: event.target.value})
+        
+    }
+
+    async submit(content) {
+    	await this.props.onSubmit(content);
+    	this.setState({content: ''})
+    }
+
+    isReady() {
+    	if (this.state.content !== '') {
+    		return 'ready';
+        }
+    }
 
 	render() {
 		return (
-			<div className="nextWord">
-			<input type="text" />
+			<div className="nextWordContainer">
+				<div className="submittedWordContainer">
+					<div className="progressBar" style={{ width: '4px'}}></div>
+					<div  className="nextWord">
+						<input type="text" placeholder="your word" name="content" value={this.state.content} onChange={this.handleContentChange.bind(this)}/>
+					</div>
+				</div>
+				<button className={this.isReady()} onClick={() => this.submit(this.state.content)}>Submit</button>
 			</div>
 		)
 	}
