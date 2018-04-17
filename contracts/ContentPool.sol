ab1eabfb3b4701e274e8b92d861c37ecb60aad81
Travis Hairfield <travis.hairfield@gmail.com>
2018-04-17 13:22:54 -0700

    finish content pool. add tests

--- contracts/ContentPool.sol
+++ contracts/ContentPool.sol
@@ -1,116 +1,69 @@
 pragma solidity ^0.4.18;
 
-
-// open questions
-// - do we need any of the getter functions?
-// => yes, the getter for the length is required
-// => no, a getter is not required to access a public array 
-// => no, a getter is not required to access a struct
-// - do we need to be able to grab an Item by its content?
-// => no, I don't really see a need for that
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+// NOTE - what I really want to do is be able to look up by owner - that will have to be done using log filters
 
 
-// - what I really want to do is be able to look up by owner - that will have to be done using log filters
+contract ContentPool is Ownable {
 
-
-contract ContentPool {
-
-	struct Item {
-		uint index;
+	struct Item 
+	{
 		address poster;
-		string content; // this connects to the hashIndexMap
+		string content;
 	}
 
-	// use this to get item by index
-	Item[] public itemList;
-	// use this to convert key into index
+	uint32 public currentPoolVersion;
+
+	mapping (uint => Item[]) public itemList;
 	mapping (bytes32 => uint) public hashIndexMap;
 
-	function newContent(string _content) public 
-	returns (bool) {
+	function ContentPool() public 
+	{
+		owner = msg.sender;
+		currentPoolVersion = 0;
+	}
 
-		Item memory newItem = Item({
-			index: itemList.length,
+	function newContent(string _content) public 
+	returns (bool) 
+	{
+		Item memory newItem = Item(
+		{
 			poster: msg.sender,
 			content: _content
 		});
 
-		// add the new item to the itemList
-		itemList.push(newItem);
+		itemList[currentPoolVersion].push(newItem);
 
-		// add the hash key to the hash => index map
-		// TODO will the mapping automatically calculate this hash? 
-		bytes32 key = sha3(_content);
-		hashIndexMap[key] = index;
+		bytes32 key = keccak256(currentPoolVersion, _content);
+		hashIndexMap[key] = itemList[currentPoolVersion].length-1;
 
 		return true;
 	}
 
-	// TODO this needs permissions
-	// TODO where should this money go?
-	function clearPool(address _recipient) public
-	returns (bool) {
-
-		selfdestruct(_recipient);
+	function getItem(uint index) public view
+	returns (address poster, string content) 
+	{
+		require(index < poolLength());
+		return (itemList[currentPoolVersion][index].poster, itemList[currentPoolVersion][index].content);
 	}
 
-	// no need for this yet
-	// TODO this needs permissions
-	function deleteContent(string _contentToDelete) public 
-	returns (bool) {
-
-		bytes32 keyToDelete = sha3(_contentToDelete);
-		uint _indexToDelete = hashIndexMap[keyToDelete];
-
-		// update the list
-		Item itemToDelete = itemList[_indexToDelete];
-		Item itemToMove = itemList[itemList.length-1];
-		itemList[_indexToDelete] = itemToMove;
-		itemList.length--;
-		
-		// update the index
-		bytes32 keyToMove = sha3(itemToMove.content);
-		hashIndexMap[keyToMove] = _indexToDelete;
-		delete hashIndexMap[keyToDelete];
-		return true;
+	function getIndex(string _content) public view
+	returns (uint) 
+	{
+		bytes32 key = keccak256(currentPoolVersion, _content);
+		return hashIndexMap[key];
 	}
 
-	// should be able to do this with contract.itemList().length()
 	function poolLength() public view 
-	returns (uint) {
-
-		return itemList.length;
+	returns (uint) 
+	{
+		return itemList[currentPoolVersion].length;
 	}
 
-	// do we need this? => no
-	// function itemByIndex(uint _index) public view 
-	// 	returns (uint index, address poster, string content) {
-
-	// 	Item item = itemList[_index];
-	// 	return (item.index, item.poster, item.content);
-	// }
-
-	// do we need this?
-	// not really => we will want to access by index and by owner, eventually by tags
-	// function itemByContent(string _content) public view
-	// 	returns (uint index, address poster, string content) {
-
-	// 	bytes32 key = sha3(_content);
-	// 	Item item = hashItemMap[key];
-	// 	return (item.index, item.poster, item.content);
-	// }
+	function clear() public onlyOwner
+	returns (bool) 
+	{
+		currentPoolVersion++;
+		return true;
+	}
 }
-
-
-// // option 1 -> better for getting items by index
-// // use this to get item by index
-// Item[] public itemList;
-// // use this to convert key to index
-// mapping (bytes32 => uint) public hashIndexMap;
-
-// // option 2 -> better for getting items by key
-// // use this to get item by key
-// mapping (bytes32 => Item) public hashItemMap;
-// // use this to convert index to key 
-// bytes32[] public itemList;
-// this way also requires us to store a uint pointer from the Item to the list
---
+++ test/testContentPool.js
@@ -0,0 +1,45 @@
+const ContentPool = artifacts.require("./ContentPool.sol");
+
+const BigNumber = web3.BigNumber;
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+contract('ContentPool', function(accounts) {
+	let pool;
+	const string = 'Applebees';
+
+	before("should store an item", async function() {
+		pool = await ContentPool.new();
+		await pool.newContent(string);
+	})
+
+	it("should retrieve by index", async function() {
+		const [address, content] = await pool.getItem(0);
+		assert.equal(content, string);
+	});
+
+	it("should retrieve by content", async function() {
+		const index = await pool.getIndex(string);
+		assert.equal(index.toNumber(), 0);
+
+		const [address, content] = await pool.getItem(index);
+		assert.equal(content, string);
+	});
+
+	it("should return the count of items stored in this version", async function() {
+		const length = await pool.poolLength();
+		assert.equal(length, 1);
+	});
+
+	it("should clear the pool", async function () {
+		await pool.clear();
+
+		const length = await pool.poolLength();
+		assert.equal(length, 0);
+
+		await pool.getItem(0).should.be.rejectedWith('revert');;
+	});
+})
