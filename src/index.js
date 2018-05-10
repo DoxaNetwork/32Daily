383eae5ff4620d8bafcdf7ed74307f782d452cc4
Travis Hairfield <travis.hairfield@gmail.com>
2018-05-10 12:09:55 -0700

    delete old files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import App from './App'
 import ThirtytwoDaily from './ThirtytwoDaily'
 
 ReactDOM.render(
-  // <App name="Travis">Hello!</App>,
   <ThirtytwoDaily></ThirtytwoDaily>,
   document.getElementById('root')
 );
