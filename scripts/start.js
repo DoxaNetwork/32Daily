30e3c18752f0c2a18e9db326e77bf9836afccb09
Travis Hairfield <travis.hairfield@gmail.com>
2018-08-17 18:58:45 -0700

    update react-scripts and scripts/start.js

--- scripts/start.js
+++ scripts/start.js
@@ -17,7 +17,8 @@ var checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 var formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 var getProcessForPort = require('react-dev-utils/getProcessForPort');
 var openBrowser = require('react-dev-utils/openBrowser');
-var prompt = require('react-dev-utils/prompt');
+const inquirer = require('inquirer');
+// var prompt = require('react-dev-utils/prompt');
 var pathExists = require('path-exists');
 var config = require('../config/webpack.config.dev');
 var paths = require('../config/paths');
@@ -299,13 +300,17 @@ detect(DEFAULT_PORT).then(port => {
   if (isInteractive) {
     clearConsole();
     var existingProcess = getProcessForPort(DEFAULT_PORT);
-    var question =
-      chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.' +
-        ((existingProcess) ? ' Probably:\n  ' + existingProcess : '')) +
-        '\n\nWould you like to run the app on another port instead?';
-
-    prompt(question, true).then(shouldChangePort => {
-      if (shouldChangePort) {
+    const question = {
+      type: 'confirm',
+      name: 'shouldChangePort',
+      message: chalk.yellow(
+        `Something is already running on port ${DEFAULT_PORT}.` +
+          `${existingProcess ? ` Probably:\n  ${existingProcess}` : ''}`
+      ) + '\n\nWould you like to run the app on another port instead?',
+      default: true,
+    };
+     inquirer.prompt(question).then(answer => {
+      if (answer.shouldChangePort) {
         run(port);
       }
     });
