425f116655f7227f23bb7e3f10de80e7e6c477ab
Travis Hairfield <travis.hairfield@gmail.com>
2018-10-24 12:37:46 -0700

    pin things after adding to ipfs

--- client/src/utils/ipfs.js
+++ client/src/utils/ipfs.js
@@ -46,6 +46,7 @@ async function fileToIPFS(readerResult) {
   const buffer = Buffer.from(readerResult)
   const response = await ipfs.add(buffer);
   const ipfsPath = response[0]['path'];
+  await ipfs.pin.add(ipfsPath);
   return ipfsPath;
 }
 
@@ -60,6 +61,7 @@ async function postToIPFS(text) {
     const ipfs = await getIPFSNode();
     const response = await ipfs.files.add(Buffer.from(text));
     const ipfsPath = response[0]['path'];
+    await ipfs.pin.add(ipfsPath);
     const ipfsPathShort = getBytes32FromIpfsHash(ipfsPath)
     return ipfsPathShort;
 }
