e7ccfd2aa5406686c9a686f16a1ffaf949692d2a
Travis Hairfield <travis.hairfield@gmail.com>
2018-11-15 13:27:07 -0800

    not all producers support that high of a gasLimit

--- truffle.js
+++ truffle.js
@@ -5,13 +5,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 8000029
+      gas: 8000000
     },
     ropsten_geth:  {
        network_id: 3,
        host: "localhost",
        port:  8545,
-       gas:   8000029
+       gas:   8000000
     }
   }
 };
